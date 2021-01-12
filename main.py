fa5d0983ab567df63d1e08611a45fff64d401921
Tummala Sai <110589@eesd.org>
2021-01-12 00:08:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#This is a comment, comments are not run by the computer
+#Output: Information that the computer prints out, what you see on the right hand side
+
+print("Hello World")
+
+# Name, grade, favorite book, and favorite superhero
+
+print("My name is Sai and I am in 7th grade","\nMy favorite book is 'Spy School' by Stuart Gibbs", "\nMy favorite superhero is Iron-Man")
+
+print("Hello")
+print("World")
+# This is different from the first print statement. The first one is printed on one line. This one is printed on two different lines.
+
+# Concatenation: add two strings together using a '+'
+
+print("Hello "+ "World")
